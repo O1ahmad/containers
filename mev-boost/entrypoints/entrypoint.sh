68c50d2fd09fec9a9436e2f6a938e5816ee4d12a
Ahmad Wilson <zer0ne.io.x@gmail.com>
2023-02-17 14:40:57 -0500

    add initial mev-boost MEV relay client container infra

---
+++ mev-boost/entrypoints/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+
+DIR=/docker-entrypoint.d
+if [[ -d "$DIR" ]] ; then
+  echo "Executing custom entrypoint scripts..."
+  /bin/run-parts --exit-on-error "$DIR"
+fi
+
+echo "Starting mev-boost - run arguments: ${RUN_ARGS:-None}"
+exec /usr/bin/tini -g -- $@ ${RUN_ARGS}
